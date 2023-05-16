42207fbb5dba553804b67d71a772885641aa3123
Ankit Maharjan <ankit.maharjan@swifttech.com>
2023-05-15 22:28:11 -0700

    first commit

---
+++ config/connection.js
@@ -0,0 +1,44 @@
+const mysql = require('mysql2');
+
+class Database {
+    constructor(config) {
+        this.connection = mysql.createConnection( process.env.JAWSDB_URL ? process.env.JAWSDB_URL : config );
+    }
+
+    query(sql, args=[]) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, args, (error, rows) => {
+                if (error) {
+                    return reject(error);
+                } else {
+                    resolve(rows);
+                }
+            });
+        });
+    }
+
+    close() {
+        return new Promise((resolve, reject) => {
+            this.connection.end( error => {
+                if (error) {
+                    return reject(error);
+                } else {
+                    resolve();
+                }
+            } );
+        } );
+    }
+};
+
+const connectDB = (dbName, dbPassword) => {
+    const db = new Database({
+      host: "localhost",
+      port: 3306,
+      user: "ankit",
+      password: "root123",
+      database: "flux_db"
+    })
+    return db;
+};
+
+module.exports = connectDB;
